10bf33d599c0e8bb30e0ae8d2ff6811695c60b09
wangq <ak5115wq@hotmail.com>
2016-03-11 15:44:17 +0800

    ass

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,71 +9,27 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    //console.log('Doing login', $scope.loginData);
-
-    var url = "flag=checkPhoneLogin&USERNAME="+$scope.loginData.username+"&PASSWORD="+$scope.loginData.password+"&callback=JSON_CALLBACK";
-
-    $http.jsonp(url,{}).success(function(data){
-      //console.log(data);
-      $scope.userInfo = data;
-      //console.log("wewefwef");
-      $scope.closeLogin();
-      //$location.path('/app/browse');
-      //$state.go('app.browse');
-      //$location.url('/app/browse');
-    });
 
 
-    // Simulate a login delay. Remove this and replace with your login code if using a login system
-    //$timeout(function() {
-    //  $scope.closeLogin();
-    //}, 1000);
-  };
+
+
+  
 })
-.controller('LoginCtrl', function($scope, $state, $http, backEndURL) {
+.controller('LoginCtrl', function($scope, $state, $http, backEndURL, $rootScope) {
   $scope.loginData = {};
 
-  $scope.doLogin = function(valid){
-
-    if(!valid){
-      $scope.errrorMessage = "dddd";
-    };
+  $scope.doLogin = function(){
 
     var url = backEndURL + "flag=checkPhoneLogin&USERNAME="+$scope.loginData.username+"&PASSWORD="+$scope.loginData.password+"&callback=JSON_CALLBACK";
 
     $http.jsonp(url,{}).success(function(data){
 
       if('error' === data[0].responseMessage){
-
-        $scope.msg = data[0].errrorMessage;
-
+        $scope.msg = data[0].errorMessage;
       } else {
-        console.log("success direct to browse");
-        $state.go('app.browse');  
+        $rootScope.user = data[0];
+        $state.go('app.browse');
       }
-      
 
     }).error(function(){
       console.log("request back end had an error happend!")
